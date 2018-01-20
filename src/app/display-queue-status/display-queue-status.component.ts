06a53ec000f7cdb822a5b1e15155517838062204
dev-0509 <devsaini.id@gmail.com>
2018-01-20 14:53:22 +0530

    Integrated Customer Window

--- src/app/display-queue-status/display-queue-status.component.ts
+++ src/app/display-queue-status/display-queue-status.component.ts
@@ -1,32 +1,3 @@
-// import { Component, OnInit, Input } from '@angular/core';
-
-// @Component({
-//     selector: 'app-display-queue-status',
-//     templateUrl: './display-queue-status.component.html',
-//     styleUrls: ['./display-queue-status.component.css']
-// })
-
-// export class DisplayQueueStatusComponent implements OnInit {
-
-//     @Input() queue_position: string;
-//     @Input() queue_id: string;
-//     @Input() accepting_appointments: boolean;
-//     @Input() appointments_flag: any;
-//     @Input() appointments: any;
-
-//     constructor() {
-//     }
-
-//     ngOnInit() {
-//     }
-
-//     onClick() {
-//         alert('Coming Soon!');
-//     }
-
-// }
-
-
 import { Component, OnInit, Input } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Response } from '@angular/http';
